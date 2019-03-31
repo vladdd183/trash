5e450665b9e29dc284fa17090c2065f2e7e916bd
vladdd183 <vladdd183@gmail.com>
2019-03-31 15:45:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
-token = "6deb20fb6651911d89e9df532424501b9f1fa1256054638ec07729934f5f928b9e7da32ed41c1e1c53d38"
-group_id = 175626312
+token = "" #Сюда воткнуть токен
+group_id = 123 #Сюда воткнуть айди группы в числовом формате
 
 vk_session = vk_api.VkApi(token=token)
 vk = vk_session.get_api()
 
 def keyboard():
     keyboard = VkKeyboard(one_time=False)
-    keyboard.add_button('Кнопка 1', color=VkKeyboardColor.DEFAULT, payload= {"button": "button1"})
+    keyboard.add_button('Кнопка 1', color=VkKeyboardColor.DEFAULT, payload= {"button": "button1"}) # Можно поменять название, колличество и цвет
     keyboard.add_line()
     keyboard.add_button('Кнопка 2', color=VkKeyboardColor.DEFAULT, payload= {"button": "button2"})
     keyboard.add_line()
@@ -24,4 +24,4 @@ longpoll = VkBotLongPoll(vk_session, group_id)
 for event in longpoll.listen():
 	if event.type == VkBotEventType.MESSAGE_NEW:
 		peer_id = int(event.obj.peer_id)
-		vk.messages.send(peer_id= peer_id, message= 'TEXT', keyboard=keyboard())
+		vk.messages.send(peer_id= peer_id, message= 'TEXT', keyboard=keyboard()) #Можно поменять текст
